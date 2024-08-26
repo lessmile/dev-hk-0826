d98a92afdc2fa3b1968f2f3503b4939c9ef02bc6
wtf <878961426@qq.com>
2024-08-26 19:14:10 +0800

    Update OrderPlugin.php

--- Plugin/OrderPlugin.php
+++ Plugin/OrderPlugin.php
@@ -13,7 +13,7 @@ class OrderPlugin
         $this->curl = $curl;
     }
 
-    public function afterSave(Order $subject, $result)
+    public function afterSave_tmp(Order $subject, $result)
     {
         if ($subject->getIsObjectNew() || $subject->dataHasChangedFor('status')) {
             $data = ['order_id' => $subject->getId(), 'status' => $subject->getStatus()];
@@ -22,4 +22,17 @@ class OrderPlugin
         }
         return $result;
     }
+
+    public function afterSave(Order $subject, $result)
+{
+    // 如果订单是新建的对象，或者订单的任何数据发生了变化
+    if ($subject->getIsObjectNew() || $subject->hasDataChanges()) {
+        $data = ['order_id' => $subject->getId(), 'status' => $subject->getStatus()];
+        $webhookUrl = 'https://enocx9j9jmkxc.x.pipedream.net/magento/dev/webhooks';
+        $this->curl->post($webhookUrl, json_encode($data));
+    }
+
+    return $result;
+}
+
 }
