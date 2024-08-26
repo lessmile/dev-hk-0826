3e84b2b3f90c6559b5d112da623b7a4659b92649
wtf <878961426@qq.com>
2024-08-26 20:12:04 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Wtftt_Webhook',
+    'Wtftt_Webhook2',
     __DIR__
 );
